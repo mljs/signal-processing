b964d51b36d31f17c64796350b338cb9aea9f911
Luc Patiny <luc@patiny.com>
2022-05-05 08:05:46 +0200

    fix: update dependencies and normed use 'value'

--- src/filters/scaling/__tests__/normed.test.ts
+++ src/filters/scaling/__tests__/normed.test.ts
@@ -6,7 +6,7 @@ import { normed } from '../normed';
 expect.extend({ toBeDeepCloseTo, toMatchCloseTo });
 
 test('normed', () => {
-  const result = normed(linear, { algorithm: 'max', max: 10 });
+  const result = normed(linear, { algorithm: 'max', value: 10 });
   expect(result.data).toMatchCloseTo({
     x: Float64Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9]),
     y: Float64Array.from([2, 4, 6, 8, 10, 8, 6, 4, 2]),
