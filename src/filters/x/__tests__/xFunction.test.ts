ebd44ed3e680772c26f86efb5bdb431bd7d76f4c
Luc Patiny <luc@patiny.com>
2022-05-06 09:44:04 +0200

    feat: add xFunction

---
+++ src/filters/x/__tests__/xFunction.test.ts
@@ -0,0 +1,34 @@
+import { toBeDeepCloseTo, toMatchCloseTo } from 'jest-matcher-deep-close-to';
+
+import linear from '../../__tests__/data/linear';
+import { xFunction } from '../xFunction';
+
+expect.extend({ toBeDeepCloseTo, toMatchCloseTo });
+
+describe('xFunction', () => {
+  it('nothing to do', () => {
+    const result = xFunction(linear, {});
+    expect(result.data).toMatchCloseTo({
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      y: [1, 2, 3, 4, 5, 4, 3, 2, 1],
+    });
+  });
+  it('x+2', () => {
+    const result = xFunction(linear, {
+      function: 'x+2',
+    });
+    expect(result.data).toMatchCloseTo({
+      x: [3, 4, 5, 6, 7, 8, 9, 10, 11],
+      y: [1, 2, 3, 4, 5, 4, 3, 2, 1],
+    });
+  });
+  it('pow(x,2)', () => {
+    const result = xFunction(linear, {
+      function: 'pow(x,2)',
+    });
+    expect(result.data).toMatchCloseTo({
+      x: [1, 4, 9, 16, 25, 36, 49, 64, 81],
+      y: [1, 2, 3, 4, 5, 4, 3, 2, 1],
+    });
+  });
+});
---
+++ src/filters/x/xFunction.ts
@@ -0,0 +1,36 @@
+import { DataXY } from 'cheminfo-types';
+import { xApplyFunctionStr } from 'ml-spectra-processing';
+
+export interface XFunctionFilter {
+  name: 'xFunction';
+  options?: XFunctionOptions;
+}
+
+export interface XFunctionOptions {
+  /**
+   * Will apply a function on the X variable on each element of the array described as a string
+   * In front of sequence of lowercase we will add 'Math.'. This allows to write
+   * `sin(x) + cos(x)` and it will be replace internally by (x) => (Math.sin(x) + Math.cos(x))
+   * @default ''
+   */
+  function?: string;
+}
+/**
+ * Filter that allows to
+ * @param data
+ * @param options
+ */
+export function xFunction(
+  data: DataXY<Float64Array>,
+  options: XFunctionOptions = {},
+) {
+  return {
+    data: {
+      x: xApplyFunctionStr(data.x, {
+        variableLabel: 'x',
+        fctString: options.function,
+      }),
+      y: data.y,
+    },
+  };
+}
